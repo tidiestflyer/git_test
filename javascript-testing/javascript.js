70aa102a1cdc9cb9a4007a409b2353cfa1701d96
tidiestflyer <35712857+tidiestflyer@users.noreply.github.com>
2026-07-25 21:34:46 -0500

    Playing around with javascript in an html file and in the js file.

---
+++ javascript-testing/javascript.js
@@ -0,0 +1,17 @@
+let firstName = "John";
+console.log(firstName);
+
+firstName = "Tim";
+console.log(firstName);
+
+const myVar = "Meow";
+//myVar = "woof";
+
+console.log(myVar);
+
+console.log(39 + 500);
+
+console.log(5 + 2 + 7 + 257 + 10 + 75);
+
+let expression = (4 + 6 + 9) / 77;
+console.log(expression);
