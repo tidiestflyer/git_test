5c52750780e83f8c4129006903fb8a280bc958e3
tidiestflyer <35712857+tidiestflyer@users.noreply.github.com>
2026-08-05 19:46:45 -0500

    Added some dom manipulation to the javascript

---
+++ dom-manipulation-events/javascript.js
@@ -0,0 +1,7 @@
+const container = document.querySelector("#container");
+
+const red = document.createElement("p");
+red.classList.add("para");
+red.textContent= "Hi, I'm red!";
+
+container.appendChild(red);
